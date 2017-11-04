cf5dd3251f6af52926f3f1dbb2e4f87573a75778
sarmstr5 <git.sarmstr5@gmail.com>
2017-11-04 16:39:45 -0400

    created mllib cv method

---
+++ src/preprocessing_add_row_ids.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+import csv
+import sys
+
+#r_fn = sys.argv[1] 
+#w_fn = "../{}_processed.csv".format(r_fn.split(".csv")[0])
+#if verbose:
+#    print(r_fn)
+#    print(w_fn)
+
+
+verbose = False
+def get_abs_file_path(file_dir, fn):
+    cur_dir = os.path.abspath(os.curdir)
+    return os.path.normpath(os.path.join(cur_dir, "..", file_dir, fn))
+
+r_fn = get_abs_file_path("test_input", "tiny.csv")
+w_fn = get_abs_file_path("test_input", "tiny_processed.csv")
+with open(w_fn, 'w') as output:
+    wr = csv.writer(output)
+    with open(r_fn, 'rt') as input:
+        header = True
+        i = 0
+        for row in csv.reader(input):
+            if verbose:
+                print(row)
+            if header:
+                wr.writerow(['row_id'] + row)
+                header = False
+            else:
+                wr.writerow([i] + row)
+            i += 1
+
--- src/sgd_lr.py
+++ src/sgd_lr.py
@@ -46,20 +46,21 @@ def create_df_from_rdd(line_list):
     return Row(flight_time=y, lat_dist=la_dist, long_dist=lo_dist)
 
 # parse the data, convert str to floats and ints as appropriate
-def parse_row(line_list):
-    # id, vendor_id, pickup_datetime, dropoff_datetime,
+def parse_row_for_cv(line_list):
+    # row_id, id, vendor_id, pickup_datetime, dropoff_datetime,
     # passenger_count, pickup_longitude, pickup_latitude, 
     # dropoff_longitude, dropoff_latitude, store_and_fwd_flag,
     # trip_duration
-    long_dist = abs(float(line_list[5]) - float(line_list[7])) # double check
-    lat_dist = abs(float(line_list[6]) - float(line_list[8]))
+    long_dist = abs(float(line_list[6]) - float(line_list[8])) # double check
+    lat_dist = abs(float(line_list[7]) - float(line_list[9]))
     y = int(line_list[-1])
+    row_id = int(line_list[0])
     #not currently using but may need some of the value
-    x_values = [line_list[0], line_list[1],  line_list[2],  line_list[3],
-              int(line_list[4]),  float(line_list[5]),  float(line_list[6]),
-              float(line_list[7]), float(line_list[8]),  line_list[9]]
+    #x_values = [line_list[0], line_list[1], line_list[2],  line_list[3],  line_list[4],
+    #          int(line_list[5]),  float(line_list[6]),  float(line_list[7]),
+    #          float(line_list[8]), float(line_list[9]),  line_list[10]]
 
-    return LabeledPoint(y, (lat_dist, long_dist))
+    return (row_id, [y, lat_dist, long_dist])
 
 def save_rdd_to_disk(output_dir, output_fn, rdd):
     output_path = get_abs_file_path(output_dir, output_fn)
@@ -83,9 +84,9 @@ def build_model(rdd):
     lr = LinearRegression(maxIter=10, regParam=0.3, solver='sgd')
     pipeline = Pipeline(stages=[lr])
     paramGrid = ParamGridBuilder() \
-                .addGrid(lr.stepSize=cv_step)
-                .addGrid(lr.miniBatchFraction=cv_batch_size)
-                .addGrid(lr.updater=cv_batch_size)
+    #            .addGrid(lr.stepSize=cv_step) \
+    #            .addGrid(lr.miniBatchFraction=cv_batch_size) \
+    #            .addGrid(lr.updater=cv_batch_size)
 
     crossval = CrossValidator(\
         estimator=pipeline, \
@@ -104,20 +105,26 @@ def build_model(rdd):
         .reduce(lambda x, y: x + y) / values_and_preds.count()
     results.append(MSE)
 
-def cross_validate(rdd, train_percent=0.8, kfolds=10 ):
-    for i in range(kfolds):
-         hold_out = rdd.sample(False,1/kfolds)
-         yield hold_out
+def cross_validate(rdd, k_folds, test_k):
+    # take mod
+    train = rdd.filter(lambda x: x[0] % k_folds != test_k) \
+               .map(lambda x: LabeledPoint(x[1][0], (x[1][1:])))
+
+    test = rdd.filter(lambda x: x[0] % k_folds == test_k)  \
+               .map(lambda x: LabeledPoint(x[1][0], (x[1][1:])))
+                    
+    return train, test
 
 def main():
     # input parameters
     if len(sys.argv) < 3:
         print("you didnt give directory inputs, using test file")
         input_dir = "test_input"
-        input_fn = "tiny.csv"
+        input_fn = "processed_tiny.csv"
         input_file_path = os.path.join(input_dir, input_fn)
         #input_file_path = get_abs_file_path(input_dir, input_fn)
         output_fn="test"
+
     else:
         input_fn = sys.argv[1]
         output_fn = sys.argv[2]
@@ -140,14 +147,12 @@ def main():
     data = data.filter(lambda x: x != header)
 
     # convert attributes to floats/ints and make key/values
-    parsed_pair_rdd = data.map(parse_row)
-    parsed_df = parsed_pair_rdd.map(create_df_from_rdd)
-    parsed_df.take(2)
 
     # attributes
     cv_step = [x / float(100) for x in range(1, 20, 5)]
-    cv_batch_fraction = [x / float(10) for x in range(1, 10, 5)]
+    cv_batch_fraction = [x / float(100) for x in range(1, 11, 5)]
     regType= ["L1", "L2"]
+    k_folds = 10
 
     mse_list = []
     step_list = []
@@ -155,27 +160,33 @@ def main():
     reg_type_type_list = []
     MSE_list = []
 
+    parsed_rdd = data.map(parse_row_for_cv).persist()
+    train_set_rdd, test_set_rdd = parsed_rdd.randomSplit([0.8, 0.2], seed=1234)
+    train_set_rdd.persist()
+    n_train = train_set_rdd.count()
+
     for step in cv_step:
         for i in range(kfolds):
-            train_hold_out = cross_validate(parsed_pair_rdd)
-            train = parsed_pair_rdd(lambda x: x not in train_hold_out)
+            train_rdd, validate_rdd = train_set_rdd.map(cross_validate)
 
             # Build model
-            lm = LinearRegressionWithSGD.train(parsed_pair_rdd, iterations=10,
+            lm = LinearRegressionWithSGD.train_rdd(train_rdd, iterations=10,
                                                step=step, miniBatchFraction=0.1,
                                                regParam=0.0, regType=None,
                                                intercept=True, validateData=True )
 
             # Evalute the model on training data
-            values_and_preds = parsed_pair_rdd.map(lambda x: (x.label,
-                                                              lm.predict(x.features)))
+            values_and_preds = train_rdd.map(lambda x: (x.label, lm.predict(x.features)))
+            # squares the error then adds all errors together divided by n
             MSE = values_and_preds \
                 .map(lambda x: (x[0] - x[1])**2) \
-                .reduce(lambda x, y: x + y) / values_and_preds.count()
+                .reduce(lambda x, y: x + y) / n_train
+
             results.append(MSE)
-    for i in zip(results,steps)
-        print("mean squar error = " + str(results))
-        fo
+            step_list.append(step)
+
+    for i in zip(results,step_list):
+        print("mean squar error = " + str(i))
 
     # Output results
     #output_dir = "output/spark"
