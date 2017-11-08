d4dc0476ea2ef21f9a3537e37e9890790ce0b6dd
sarmstr5 <git.sarmstr5@gmail.com>
2017-11-07 21:16:06 -0500

    still running into issues from spark output to test trail text

--- src/run_sgd.sh
+++ src/run_sgd.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-fns='1000 10000 50000 100000 500000 1000000 1458645'
+fns='1000 10000 50000 100000 500000 1458645 1000000 '
 test_fns='test_input/thousand_processed.csv'
 hdfs_dir='data/'
 output_fn=test_results.csv
+
 home_dir=$(eval echo "~$USER")
-bash_time_output="$home_dir/hw3/results/time_trials.txt"
+time_file="$home_dir/hw3/results/time_trials.txt"
+time_file2="$home_dir/hw3/results/time_trials2.txt"
+
+
 
 # might be better to save time results as a var then get append as row to csv
 for fn in $fns
 do
-  echo "$hdfs_dir$fn"
-  echo "#" $fn >> $bash_time_output
-  { time spark-submit --master yarn --deploy-mode client sgd_lr.py "$hdfs$fn" $output_fn 2>&1; } 2>> $bash_time_output
+
+  { /usr/bin/time -f "%e,%U,%S" spark-submit --master yarn --deploy-mode client sgd_lr.py "$hdfs$fn" $output_fn 2>&1; } 2>> $time_file2 
+  printf "%s,%s\n" $fn >> $time_file
+
 done
+
+  #run_time=$({ /usr/bin/time -f "%e,%U,%S" sleep 1 2>&1;} )
+# { time python $p_code $input_fn $output_fn 2>&1; } 2>> $bash_time_output
